9b5f49a649e356871c1d4817018e26fb70d7ffc9
KEINOS <github@keinos.com>
2019-09-08 13:36:01 +0900

    fix: test failure. php-alpine changed the way to install.

--- get-url-php-alpine-repo.sh
+++ get-url-php-alpine-repo.sh
@@ -8,15 +8,26 @@
 # User name of the repository
 name_subject='php-alpine'
 
-# Get Repositories
-name_repo=$(curl -s "https://api.bintray.com/repos/${name_subject}/" | jq -r '.[].name' | tail -1)
+# Get Repository name (target Alpine version)
+name_repo=$(curl -s "https://api.bintray.com/repos/${name_subject}/" | jq -r '.[1].name')
 
-# Get Packages
-name_package=$(curl -s "https://api.bintray.com/repos/${name_subject}/${name_repo}/packages" | jq -r '.[].name' | tail -1)
+# Get Package name (target PHP version)
+name_package=$(curl -s "https://api.bintray.com/repos/${name_subject}/${name_repo}/packages" | jq -r '.[].name' | grep php | head -1)
 
-# Generate URL and file
+# Generate URL of the php-alpine repository
 # Format: @php https://dl.bintray.com/:subject/:repo/:package
 # Sample: @php https://dl.bintray.com/php-alpine/v3.9/php-7.3
 url_repo_apk_add="@php https://dl.bintray.com/${name_subject}/${name_repo}/${name_package}"
-
 echo $url_repo_apk_add > /etc_apk_repositories
+
+echo '- List of apk repositories:'
+cat /etc_apk_repositories
+
+# Get PHP-Alpine project public key to trust their packages.
+#   See: https://github.com/codecasts/php-alpine
+curl -O https://dl.bintray.com/php-alpine/key/php-alpine.rsa.pub  && \
+cp php-alpine.rsa.pub /etc/apk/keys/php-alpine.rsa.pub && \
+echo '- rsa.pub key of php-alpine:' && \
+cat /etc/apk/keys/php-alpine.rsa.pub
+
+echo '- Finished running:' $0
